cefe42fb486468b3e67ccbb7e1a2b31cec47196d
ZackTO <zackstoronto@gmail.com>
2020-03-31 15:00:44 -0400

    La taille de la queue est <mon chiffre ICI>

--- b000000000.py
+++ b000000000.py
@@ -3,9 +3,32 @@
 
 @author: ZackTO
 """
+eleves = {}
+def personne_elue(name):
+    return name == "Zoureni"
+from collections import deque
 
-def main():
-  print('Informatique: le rêve')
+
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
+
+def search(name):
+  search_queue = deque()
+  search_queue += eleves [name]
+  print( len(search_queue) )
+  return False
 
 if __name__== "__main__":
-  main()
+  search("Boris")
